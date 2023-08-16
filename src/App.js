9d1b1027f7137a1b08961bafab5db519e57e2dc5
brightkingpin <luvandabright781@gmail.com>
2023-08-16 11:22:33 +0300

    Done with assignment

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import StudentModule from './components/StudentModule';
-import TutorModule from './components/TutorModule'; // Import TutorModule
-import DomainModel from './components/DomainModel'; // Import DomainModel
+import TutorModule from './components/TutorModule';
+import DomainModel from './components/DomainModel';
 import AIEngine from './services/AIEngine';
+import Login from './components/Login'; // Import the Login component
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const aiEngine = new AIEngine();
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // State to manage authentication
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <Router>
       <div>
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/student/:subject"
-            element={<StudentModule aiEngine={aiEngine} />}
-          />
-          <Route
-            path="/tutor/:subject" // Modify the path
-            element={<TutorModule aiEngine={aiEngine} />} // Pass aiEngine to TutorModule
-          />
-          <Route path="/domain" element={<DomainModel />} />
-        </Routes>
+        {/* Conditionally render based on isLoggedIn state */}
+        {isLoggedIn ? (
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/student/:subject" element={<StudentModule aiEngine={aiEngine} />} />
+            <Route path="/tutor/:subject" element={<TutorModule aiEngine={aiEngine} />} />
+            <Route path="/domain" element={<DomainModel />} />
+          </Routes>
+        ) : (
+          <Login onLogin={handleLogin} /> // Render the Login component
+        )}
       </div>
     </Router>
   );
---
+++ src/components/Login.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = () => {
+    // I have implemented my authentication logic here
+    if (username === 'lecturer@gmail.com' && password === '123456') {
+      onLogin(); // I Called the onLogin function to be passed as a prop
+    } else {
+      setError('Invalid credentials');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default Login;
